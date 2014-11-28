a409a914c366d6e27f2525cefb06e76ccabc9d6c
antonve <anton.van.eechaute@student.howest.be>
2014-11-28 20:50:25 +0100

    Set up UI

---
+++ nmct.ba.cashlessproject/nmct.ba.cashlessproject.ui/ViewModel/HomeVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.ba.cashlessproject.ui.ViewModel
+{
+    class HomeVM : ObservableObject, IPage
+    {
+        public HomeVM()
+        {
+        }
+
+        public string Name
+        {
+            get { return "Home"; }
+        }
+    }
+}
