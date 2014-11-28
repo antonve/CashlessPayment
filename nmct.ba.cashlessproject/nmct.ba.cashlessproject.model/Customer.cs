9eb4d5f628d144cf5f489da1417fa6f20135936d
antonve <anton.van.eechaute@student.howest.be>
2014-11-29 00:46:43 +0100

    Added model project

---
+++ nmct.ba.cashlessproject/nmct.ba.cashlessproject.model/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.ba.cashlessproject.model
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public byte[] Picture { get; set; }
+        public double Balance { get; set; }
+    }
+}
