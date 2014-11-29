bbc577b58851357c4b9e148ccdddb6e2957a24a3
antonve <anton.van.eechaute@student.howest.be>
2014-11-29 03:12:37 +0100

    Added api project

---
+++ nmct.ba.cashlessproject/nmct.ba.cashlessproject.api/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using nmct.ba.cashlessproject.api.Models;
+using nmct.ba.cashlessproject.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace nmct.ba.cashlessproject.api.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET: api/product
+        public List<Product> Get()
+        {
+            return ProductDA.GetProducts();
+        }
+
+        // GET: api/product/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/product
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/product/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/product/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ nmct.ba.cashlessproject/nmct.ba.cashlessproject.api/Models/ProductDA.cs
@@ -0,0 +1,40 @@
+﻿using nmct.ba.cashlessproject.api.helper;
+using nmct.ba.cashlessproject.model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace nmct.ba.cashlessproject.api.Models
+{
+    public class ProductDA
+    {
+        public static List<Product> GetProducts()
+        {
+            List<Product> list = new List<Product>();
+
+            string sql = "SELECT ID, ProductName, Price FROM Products";
+            DbDataReader reader = Database.GetData("ConnectionString", sql);
+
+            while (reader.Read())
+            {
+                list.Add(Create(reader));
+            }
+            reader.Close();
+
+            return list;
+        }
+
+        private static Product Create(IDataRecord record)
+        {
+            return new Product()
+            {
+                ID = Int32.Parse(record["ID"].ToString()),
+                ProductName = record["ProductName"].ToString(),
+                Price = Double.Parse(record["Price"].ToString())
+            };
+        }
+    }
+}
