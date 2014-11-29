47fb22f5fe5c1e8b1523757f70e814e7518571f9
antonve <anton.van.eechaute@student.howest.be>
2014-11-29 04:13:06 +0100

    Product logic

--- nmct.ba.cashlessproject/nmct.ba.cashlessproject.ui/ViewModel/ManagementProductsVM.cs
+++ nmct.ba.cashlessproject/nmct.ba.cashlessproject.ui/ViewModel/ManagementProductsVM.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using Newtonsoft.Json;
+using nmct.ba.cashlessproject.model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace nmct.ba.cashlessproject.ui.ViewModel
 {
@@ -10,12 +16,88 @@ namespace nmct.ba.cashlessproject.ui.ViewModel
     {
         public ManagementProductsVM()
         {
-
+            GetProducts();
         }
 
         public string Name
         {
             get { return "Products"; }
+        }
+
+        private Product currentProduct;
+        public Product CurrentProduct
+        {
+            get { return currentProduct; }
+            set { currentProduct = value; SaveMode = true;  OnPropertyChanged("CurrentProduct"); }
+        }
+
+        private ObservableCollection<Product> _products;
+        public ObservableCollection<Product> Products
+        {
+            get { return _products; }
+            set { _products = value; OnPropertyChanged("Products"); }
+        }
+
+        private async void GetProducts()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("http://localhost:46080/api/product");
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    Products = JsonConvert.DeserializeObject<ObservableCollection<Product>>(json);
+                }
+            }
+        }
+
+        public ICommand NewProductCommand
+        {
+            get { return new RelayCommand(NewProduct); }
+        }
+
+        private void NewProduct()
+        {
+            CurrentProduct = new Product();
+            SaveMode = false;
+        }
+
+        public ICommand SaveProductCommand
+        {
+            get { return new RelayCommand(SaveProduct); }
+        }
+
+        // false = save, true = update
+        private bool SaveMode = false;
+
+        private async void SaveProduct()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string json = JsonConvert.SerializeObject(CurrentProduct);
+                HttpResponseMessage response;
+
+                if (SaveMode)
+                {
+                    response = await client.PutAsync("http://localhost:46080/api/product/" + CurrentProduct.ID.ToString(), new StringContent(json, Encoding.UTF8, "application/json"));
+                }
+                else
+                {
+                    response = await client.PostAsync("http://localhost:46080/api/product", new StringContent(json, Encoding.UTF8, "application/json"));
+                }
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonresponse = await response.Content.ReadAsStringAsync();
+                    int result = JsonConvert.DeserializeObject<int>(jsonresponse);
+
+                    if (result == 1)
+                    {
+                        GetProducts();
+                        CurrentProduct = null;
+                    }
+                }
+            }
         }
     }
 }
